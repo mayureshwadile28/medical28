18bf716c4e9dcb6d54c413479aaee2d48a9107d4
may wadile <maywa554785@gmail.com>
2025-11-10 10:49:52 +0000

    don't use AI delete the AI use OCR offline mode

--- src/app/actions.ts
+++
@@ -1,9 +0,0 @@
-'use server';
-
-import { extractBatchDetails, type BatchDetailsInput, type BatchDetailsOutput } from '@/ai/flows/extract-batch-details-flow';
-
-export async function extractBatchDetailsAction(
-  input: BatchDetailsInput,
-): Promise<BatchDetailsOutput> {
-  return await extractBatchDetails(input);
-}
