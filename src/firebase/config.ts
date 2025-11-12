53466a70db73259e47c89bc8ef6d183eb505c075
may wadile <maywa554785@gmail.com>
2025-11-12 15:51:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7126454527-19af6",
+  "appId": "1:162687507975:web:ff895110f5d711c821d4a2",
+  "apiKey": "AIzaSyCz3djgF7z7ongFY2iIlCuQpuKnnhnDIvE",
+  "authDomain": "studio-7126454527-19af6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "162687507975"
+};
