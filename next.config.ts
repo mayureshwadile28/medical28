1267f4f2ae39dc8855e7d96b0af71b99a4df0139
may wadile <maywa554785@gmail.com>
2025-11-14 16:12:54 +0000

    i got the error on vercel Failed to compile. ./next.config.ts:23:5 Type

--- next.config.ts
+++ next.config.ts
@@ -2,28 +2,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-        ],
-      },
-    ];
-  },
-  devIndicators: {
-    allowedDevOrigins: [
-      'https://*.cloudworkstations.dev',
-    ]
-  },
   images: {
     remotePatterns: [
       {
